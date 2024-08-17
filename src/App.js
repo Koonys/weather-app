a77255f3bb1aeef1ac670a32d0b792f035596e03
SeokHoon <sughoon1@gmail.com>
2024-08-17 14:18:40 +0900

    11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useEffect, useState} from "react";
+import WeatherBox from "./component/WeatherBox";
+import WeatherButton from "./component/WeatherButton";
+const apikey = "adf4874664a79865e67278f1ec5bef32"
 
 function App() {
+
+  const [weather, setWeather] = useState(null);
+  const getCurrentLocation=()=>{
+      navigator.geolocation.getCurrentPosition((position)=>{
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        getWeatherByCurrentLocation(lat,lon)
+      });
+  }
+
+  const getWeatherByCurrentLocation = async (lat,lon) =>{
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+  }
+
+  useEffect(() => {
+    getCurrentLocation()
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"container"}>
+        <WeatherBox weather={weather}></WeatherBox>
+        <WeatherButton></WeatherButton>
+      </div>
     </div>
   );
 }
